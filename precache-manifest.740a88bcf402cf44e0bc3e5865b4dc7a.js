dd70830c4d5f40f04b4c579cd3f1f70e003ed16d
ruannicolini <ruan_03@hotmail.com>
2020-08-16 14:46:40 -0300

    Updates

---
+++ precache-manifest.740a88bcf402cf44e0bc3e5865b4dc7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1193aa48c8ed6f0a1d7dff35f94abf3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dd537281790a05908d0",
+    "url": "/static/js/2.8c6c8f29.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8c6c8f29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d99200298d5172cfa87",
+    "url": "/static/js/main.7225afdc.chunk.js"
+  },
+  {
+    "revision": "3c51a4347d5c71b72974",
+    "url": "/static/js/runtime-main.94e55ade.js"
+  },
+  {
+    "revision": "88c09a04fab5744ba015c513b99dbb84",
+    "url": "/static/media/bow.88c09a04.png"
+  },
+  {
+    "revision": "677aa2be793551a0f1afd6d89f8349bd",
+    "url": "/static/media/logoSmall2.677aa2be.png"
+  }
+]);
